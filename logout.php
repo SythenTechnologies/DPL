ca99051203a5b478c4b0a9ad8c1f116166443072
FireByTrial <james.oliver59@gmail.com>
2013-06-09 17:12:44 -0600

    Improved Login, corrected Dev Logout

--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php
 	session_start();
 	session_destroy();
-	echo "You have been logged out <br /><br /><a href=index.php>Click Here to return to Login</a>";
-	header('Location: /');
+	echo "You have been logged out <br /><br /><a href=./Security/Login.html>Click Here to return to Login</a>";
+    //$LOC = $_SERVER['HTTP_HOST'];
+	header("Location: ./Security/Login.html?r=0");
 ?>
